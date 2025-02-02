577c119d2da15d89d1552683e9cc692f8400d541
Adaylla <a.adaylla@escolar.ifrn.edu.br>
2025-02-02 17:36:52 -0300

    corrigindo erros

--- usuarios/models.py
+++ usuarios/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    matricula = models.CharField(max_length=20, unique=True)
-    turno = models.CharField(max_length=20, choices=[('matutino', 'Matutino'), ('vespertino', 'Vespertino'), ('noturno', 'Noturno')])
-    serie = models.CharField(max_length=10)
+    matricula = models.CharField(max_length=20, blank=True, null=True)
+    turno = models.CharField(max_length=20, choices=[('matutino', 'Matutino'), ('vespertino', 'Vespertino'), ('noturno', 'Noturno')], blank=True)
+    serie = models.CharField(max_length=10, blank=True)
     
     def __str__(self):
         return self.username
