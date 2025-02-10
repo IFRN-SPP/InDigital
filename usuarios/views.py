cd4c100f655271c2c7056438076a5eeb5075270d
Adaylla <a.adaylla@escolar.ifrn.edu.br>
2025-02-10 16:52:09 -0300

    autorização

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,17 +1,14 @@
 from django.shortcuts import render, redirect
 from .forms import CadastroForm
+from django.contrib import messages
 
 def cadastro(request):
-    if request.user.is_authenticated:
-        return redirect('index')
-    context = {}
     if request.method == 'POST':
         form = CadastroForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Usuário cadastrado com sucesso!')
             return redirect('login')
     else:
-        context = {
-            'form': CadastroForm(),
-        }
-    return render(request, 'cadastro.html', context)
+        form = CadastroForm()
+    return render(request, 'cadastro.html', {'form': form})
