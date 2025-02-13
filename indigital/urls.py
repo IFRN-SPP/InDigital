457294283642700eb8a46cb086be9c2071a21ad7
Adaylla <a.adaylla@escolar.ifrn.edu.br>
2025-02-13 16:18:55 -0300

    modificando algumas páginas

--- indigital/urls.py
+++ indigital/urls.py
@@ -7,18 +7,12 @@ urlpatterns = [
     path('criar/reserva/', views.criar_reserva, name='criar_reserva'),
     path('reserva/<int:reserva_id>/editar', views.editar_reserva, name='editar_reserva'),
     path('reserva/<int:reserva_id>/excluir', views.excluir_reserva, name="excluir_reserva"),
-
-
     path('reserva/', views.reserva, name='reserva'),
     path('reservar/<int:reserva_id>/', views.reservar_laboratorio, name='reservar_laboratorio'),
     path('minhas/reservas/', views.minhas_reservas, name='minhas_reservas'),
     path('cancelar_reserva/<int:reserva_id>/', views.cancelar_reserva, name='cancelar_reserva'),
-
     path('esqueceuasenha', views.esqueceuasenha, name='esqueceuasenha'),
     path('perfil', views.perfil, name='perfil'),
     path('confirmacaodasenha', views.confirmacaodasenha, name='confirmacaodasenha'),
     path('editar_perfil', views.editar_perfil, name='editar_perfil'),
-    path('contaexcluida', views.contaexcluida, name='contaexcluida'),
-    path('editar_reserva', views.editar_reserva, name='editar_reserva'),
-    path('cancelar_reserva', views.cancelar_reserva, name='cancelar_reserva'),
 ]
