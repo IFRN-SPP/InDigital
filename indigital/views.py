fe78c277cbef55f30f2e16c1caa081a3cd365bcb
Adaylla <a.adaylla@escolar.ifrn.edu.br>
2025-02-15 11:45:45 -0300

    trabalhando... crud

--- indigital/views.py
+++ indigital/views.py
@@ -87,19 +87,25 @@ def criar_laboratorio(request):
 @permission_required('indigital.editar_laboratorio', raise_exception=True)
 def editar_laboratorio(request, laboratorio_id):
     laboratorio = get_object_or_404(Laboratorio, id=laboratorio_id)
+    context = {
+        "reserva" : laboratorio,
+        "form" : LaboratorioForm(instance=laboratorio),
+        "laboratorios": Laboratorio.objects.all()
+    }
+
     if request.method == 'POST':
         form = LaboratorioForm(request.POST, instance=laboratorio)
         if form.is_valid():
             form.save()
-            return redirect('listar_laboratorio')
+            return redirect('listar_laboratorios')
         else:
-            form = LaboratorioForm(instance=laboratorio)
-    return render(request, "editar_laboratorio.html", {'form': form})
+            context["form"] = form
+    return render(request, "editar_laboratorio.html", context)
 
 @login_required
-def listar_laboratorio(request):
+def listar_laboratorios(request):
     laboratorios = Laboratorio.objects.all()
-    return render(request, "listar_laboratorio.html", {'laboratorios': laboratorios})
+    return render(request, "listar_laboratorios.html", {'laboratorios': laboratorios})
 
 @login_required
 @permission_required('indigital.excluir_laboratorio', raise_exception=True)
@@ -107,7 +113,7 @@ def excluir_laboratorio(request, laboratorio_id):
     laboratorio = get_object_or_404(Laboratorio, id=laboratorio_id)
     if request.method == "POST":
         laboratorio.delete()
-        return redirect('listar_laboratorio')
+        return redirect('listar_laboratorios')
     else:
         return render(request, "excluir_laboratorio.html", {'laboratorio': laboratorio})
     
